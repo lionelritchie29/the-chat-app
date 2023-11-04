4dc04e97222c6732b1280847525fdc7fb5d84b7e
Lionel Ritchie <lionelrtchieee@gmail.com>
2023-11-04 20:23:53 +0700

    feat: remove connection state banner

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,11 @@
 import "../index.css";
-import { useEffect, useState } from "react";
-import { socket } from "../socket";
-import ConnectionStateBanner from "../components/ConnectionStateBanner";
 import ChatList from "../components/chats/ChatList/ChatList";
 import ChatBox from "../components/chats/ChatBox/ChatBox";
 
 function Index() {
-  const [connected, setConnected] = useState(false);
-
-  useEffect(() => {
-    socket.on("connect", () => setConnected(true));
-    socket.on("disconnect", () => setConnected(false));
-
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-    };
-  }, []);
-
   return (
     <main>
-      <ConnectionStateBanner connected={connected} />
-
-      <section className="flex divide-x h-[calc(100vh-28px)]">
+      <section className="flex divide-x h-screen">
         <ChatList widthClass="w-fit" />
         <ChatBox widthClass="flex-grow" />
       </section>
